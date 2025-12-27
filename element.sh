aadf04d87b638feacdfa0948a8186473a7a58f14
sleepysecret <alsalam12999@gmail.com>
2025-12-27 05:29:55 -0500

    chore: final clean up

--- element.sh
+++ element.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
-# Element lookup script for periodic table project
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-# Check if no argument is provided
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
   exit
 fi
 
-# Query the element by atomic_number, symbol, or name
-ELEMENT=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius
-FROM elements e
-JOIN properties p ON e.atomic_number=p.atomic_number
-JOIN types t ON p.type_id=t.type_id
-WHERE e.atomic_number='$1' OR e.symbol='$1' OR e.name='$1'")
+ELEMENT=$1
 
-# If element not found
-if [[ -z $ELEMENT ]]
+# Check if ELEMENT is an integer (atomic_number)
+if [[ $ELEMENT =~ ^[0-9]+$ ]]
+then
+  ELEMENT_INFO=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius 
+  FROM elements e 
+  JOIN properties p ON e.atomic_number = p.atomic_number 
+  JOIN types t ON p.type_id = t.type_id
+  WHERE e.atomic_number=$ELEMENT;")
+else
+  # Otherwise, check symbol or name
+  ELEMENT_INFO=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius 
+  FROM elements e 
+  JOIN properties p ON e.atomic_number = p.atomic_number 
+  JOIN types t ON p.type_id = t.type_id
+  WHERE e.symbol='$ELEMENT' OR e.name='$ELEMENT';")
+fi
+
+# If not found
+if [[ -z $ELEMENT_INFO ]]
 then
   echo "I could not find that element in the database."
   exit
 fi
 
-# Parse the data
-IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING <<< "$ELEMENT"
-
-# Output the information
-echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+# Format output
+echo $ELEMENT_INFO | while IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING
+do
+  echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+done
